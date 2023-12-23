fbd347f6aeb41376de926135d4e8f43bfa96b01b
fred <domaine75@gmail.com>
2023-12-23 18:37:05 +0100

    add obsolete attribute to obsolete unit tests

--- PasswordGenerator.Tests/ObsoleteTests.cs
+++ PasswordGenerator.Tests/ObsoleteTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System.Text.RegularExpressions;
+using System;
 
 
 namespace PasswordGenerator.Tests
@@ -7,58 +8,64 @@ namespace PasswordGenerator.Tests
   public class ObsoleteTests
   {
     [Test]
-    [System.Obsolete]
+    [Obsolete]
     public void PasswordGenerator_GivenNoSettings_ShouldReturn16Length()
     {
-      PasswordGenerator pwdGen = new PasswordGenerator();
+      var pwdGen = new PasswordGenerator();
       string result = pwdGen.Next();
       Assert.AreEqual(16, result.Length);
     }
 
     [Test]
+    [Obsolete]
     public void PasswordGenerator_GivenLength3_ShouldReturnLengthErrorMessage()
     {
-      PasswordGenerator pwdGen = new PasswordGenerator(3);
+      var pwdGen = new PasswordGenerator(3);
       string result = pwdGen.Next();
       Assert.AreEqual("Password length invalid. Must be between 4 and 256 characters long", result);
     }
 
     [Test]
+    [Obsolete]
     public void PasswordGenerator_GivenLength257_ShouldReturnLengthErrorMessage()
     {
-      PasswordGenerator pwdGen = new PasswordGenerator(257);
+      var pwdGen = new PasswordGenerator(257);
       string result = pwdGen.Next();
       Assert.AreEqual("Password length invalid. Must be between 4 and 256 characters long", result);
     }
 
     [Test]
+    [Obsolete]
     public void PasswordGenerator_GivenLength256_ShouldReturn256Length()
     {
-      PasswordGenerator pwdGen = new PasswordGenerator(256);
+      var pwdGen = new PasswordGenerator(256);
       string result = pwdGen.Next();
       Assert.AreEqual(256, result.Length);
     }
 
     [Test]
+    [Obsolete]
     public void PasswordGenerator_IncludeLowercase_ShouldReturn16Length()
     {
-      PasswordGenerator pwdGen = new PasswordGenerator().IncludeLowercase();
+      var pwdGen = new PasswordGenerator().IncludeLowercase();
       string result = pwdGen.Next();
       Assert.AreEqual(16, result.Length);
     }
 
     [Test]
+    [Obsolete]
     public void PasswordGenerator_LengthRequired50_ShouldReturn50Length()
     {
-      PasswordGenerator pwdGen = new PasswordGenerator().LengthRequired(50);
+      var pwdGen = new PasswordGenerator().LengthRequired(50);
       string result = pwdGen.Next();
       Assert.AreEqual(50, result.Length);
     }
 
     [Test]
+    [Obsolete]
     public void PasswordGenerator_16DigitNumeric_ShouldReturn16DigitNumericOnlyPassword()
     {
-      PasswordGenerator pwdGen = new PasswordGenerator().IncludeNumeric();
+      var pwdGen = new PasswordGenerator().IncludeNumeric();
       var result = pwdGen.Next();
       var pattern = @"^\d{16}$";
       var m = Regex.Match(result, pattern, RegexOptions.IgnoreCase);
@@ -66,9 +73,10 @@ namespace PasswordGenerator.Tests
     }
 
     [Test]
+    [Obsolete]
     public void PasswordGenerator_16DigitLowercase_ShouldReturn16DigitLowercaseOnlyPassword()
     {
-      PasswordGenerator pwdGen = new PasswordGenerator().IncludeLowercase();
+      var pwdGen = new PasswordGenerator().IncludeLowercase();
       var result = pwdGen.Next();
       var pattern = @"^[a-z]{16}$";
       var m = Regex.Match(result, pattern, RegexOptions.IgnoreCase);
