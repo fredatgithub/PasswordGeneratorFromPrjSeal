62904ded2107120d55e5d060b0c870053af8a83d
fred <domaine75@gmail.com>
2023-12-23 18:34:32 +0100

    clean code

--- PasswordGenerator/IPassword.cs
+++ PasswordGenerator/IPassword.cs
@@ -2,15 +2,15 @@ using System.Collections.Generic;
 
 namespace PasswordGenerator
 {
-    public interface IPassword
-    {
-        IPassword IncludeLowercase();
-        IPassword IncludeUppercase();
-        IPassword IncludeNumeric();
-        IPassword IncludeSpecial();
-        IPassword IncludeSpecial(string specialCharactersToInclude);
-        IPassword LengthRequired(int passwordLength);
-        string Next();
-        IEnumerable<string> NextGroup(int numberOfPasswordsToGenerate);
-    }
+  public interface IPassword
+  {
+    IPassword IncludeLowercase();
+    IPassword IncludeUppercase();
+    IPassword IncludeNumeric();
+    IPassword IncludeSpecial();
+    IPassword IncludeSpecial(string specialCharactersToInclude);
+    IPassword LengthRequired(int passwordLength);
+    string Next();
+    IEnumerable<string> NextGroup(int numberOfPasswordsToGenerate);
+  }
 }
--- PasswordGenerator/PasswordGeneratorSettings.cs
+++ PasswordGenerator/PasswordGeneratorSettings.cs
@@ -1,12 +1,11 @@
 ﻿namespace PasswordGenerator
 {
-    public class PasswordGeneratorSettings : PasswordSettings
+  public class PasswordGeneratorSettings : PasswordSettings
+  {
+    public PasswordGeneratorSettings(bool includeLowercase, bool includeUppercase, bool includeNumeric, bool includeSpecial, int passwordLength, int maximumAttempts, bool usingDefaults)
+        : base(includeLowercase, includeUppercase, includeNumeric,
+            includeSpecial, passwordLength, maximumAttempts, usingDefaults)
     {
-        public PasswordGeneratorSettings(bool includeLowercase, bool includeUppercase, bool includeNumeric, 
-            bool includeSpecial, int passwordLength, int maximumAttempts, bool usingDefaults) 
-            : base(includeLowercase, includeUppercase, includeNumeric, 
-                includeSpecial, passwordLength, maximumAttempts, usingDefaults)
-        {
-        }
     }
+  }
 }
